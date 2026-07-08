44a542b72d9e9dbdcaa77a603fa4e3f08fc2b563
Morelaa <morelamjs322@gmail.com>
2026-07-08 09:39:29 +0700

    Upload via WhatsApp Bot

---
+++ Plugins-ESM/owner/restart.js
@@ -0,0 +1,11 @@
+'use strict';
+const handler = async (m) => {
+    await m.reply('🔄 Merestart bot...');
+    setTimeout(() => process.exit(0), 500);
+};
+handler.help = ['restart'];
+handler.tags = ['owner'];
+handler.command = /^(restart|reboot)$/i;
+handler.owner = true;
+handler.ignoreRateLimit = true;
+export default handler;
