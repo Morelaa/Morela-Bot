fddf4759339799d109385746ddba987165c3be41
Morelaa <morelamjs322@gmail.com>
2026-07-08 09:39:41 +0700

    Upload via WhatsApp Bot

---
+++ System/selfmode.js
@@ -0,0 +1,20 @@
+'use strict';
+import * as kv from '../Database/kvstore.js';
+import config from '../config.js';
+const KEY = 'selfmode:enabled';
+export function isSelfModeOn() {
+    const val = kv.get(KEY);
+    return val === undefined ? config.defaultSelfMode : val === true || val === 'true';
+}
+export function setSelfMode(enabled) {
+    kv.set(KEY, !!enabled);
+    return isSelfModeOn();
+}
+export function toggleSelfMode() {
+    return setSelfMode(!isSelfModeOn());
+}
+
+export function shouldBlockNonOwner(isOwnerSender) {
+    return isSelfModeOn() && !isOwnerSender;
+}
+export default { isSelfModeOn, setSelfMode, toggleSelfMode, shouldBlockNonOwner };
