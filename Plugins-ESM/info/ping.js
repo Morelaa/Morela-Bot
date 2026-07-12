6107d7d33c460bee2b9e2f5eca759747d1b81a6c
Morelaa <morelamjs322@gmail.com>
2026-07-12 18:02:00 +0700

    Upload via WhatsApp Bot

--- Plugins-ESM/info/ping.js
+++ Plugins-ESM/info/ping.js
@@ -1,28 +1,20 @@
 'use strict';
 import { getProcessUptime } from '../../Library/system.js';
-
 const handler = async (m, { conn }) => {
-
     let rawTime = m.timestamp || m.messageTimestamp;
-
     let time = rawTime;
     if (typeof rawTime === 'object' && rawTime !== null && typeof rawTime.toNumber === 'function') {
         time = rawTime.toNumber();
     } else {
         time = Number(rawTime);
     }
-
     let latency = Date.now() - (time * 1000);
-
     if (Number.isNaN(latency) || latency < 0) {
         latency = Math.floor(Math.random() * 20) + 1;
     }
-
     await m.reply(`🏓 *Pong!*\nLatency: ${latency}ms\nUptime proses: ${getProcessUptime()}`);
 };
-
 handler.help = ['ping'];
 handler.tags = ['info'];
 handler.command = /^(ping|speed)$/i;
-
 export default handler;
