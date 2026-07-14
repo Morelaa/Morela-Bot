ed772294349ce606036805cc20e2cbec82842bce
Morelaa <morelamjs322@gmail.com>
2026-07-14 10:13:23 +0700

    Upload via WhatsApp Bot

---
+++ Plugins-ESM/tools/limit.js
@@ -0,0 +1,27 @@
+'use strict';
+import config from '../../config.js';
+import db from '../../Database/db.js';
+import usagelimit from '../../Database/usagelimit.js';
+
+const handler = async (m) => {
+    const botName = config.botName;
+    const userJid = m.sender;
+    const isPrem = !!db.getUser(userJid)?.premium;
+    const limit = config.defaultUsageLimit;
+    const used = usagelimit.getUsedToday(userJid);
+    const sisa = isPrem ? '∞' : Math.max(limit - used, 0);
+    const pemakaian = isPrem ? `${used}/∞` : `${used}/${limit}`;
+    await m.reply(
+        `╭╌╌⬡「  *ʟɪᴍɪᴛ ᴋᴀᴍᴜ* 」\n┃\n` +
+        `┃ ◦  Terpakai : *${pemakaian}*\n` +
+        `┃ ◦  Sisa     : *${sisa}*\n` +
+        `┃ ◦  Status   : *${isPrem ? 'Premium' : 'Free'}*\n┃\n` +
+        `╰╌╌⬡\n\n꒰ © ${botName} ꒱`
+    );
+};
+handler.help = ['limit'];
+handler.tags = ['tools'];
+handler.noLimit = true;
+handler.noRegisterGate = true;
+handler.command = /^(limit|mylimit)$/i;
+export default handler;
